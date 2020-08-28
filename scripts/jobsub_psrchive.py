c9fbc1f221a7510ccd7d9e90477706ef6bd320d6
keegansmith21 <keegan.r.s21@gmail.com>
2020-08-28 17:18:09 +0800

    Fixed some typos

--- scripts/jobsub_psrchive.py
+++ scripts/jobsub_psrchive.py
@@ -3,6 +3,7 @@
 import logging
 import argparse
 import subprocess
+import datetime
 
 from config_vcs import load_config_file
 from job_submit import submit_slurm
@@ -12,40 +13,44 @@ logger = logging.getLogger(__name__)
 
 
 def make_batch(kwargs):
-    SLURM_TMPL = """{shebag}
-    #SBATCH --export={export}
-    #SBATCH --output={outfile}
-    {account}
-    #SBATCH --clusters={cluster}
-    #SBATCH --partition={partition}
-    #SBATCH --mem-per-cpu={mem}MB
+    SLURM_TMPL = """{shebang}
+#SBATCH --export={export}
+#SBATCH --output={outfile}
+{account}
+#SBATCH --clusters={cluster}
+#SBATCH --partition={partition}
+#SBATCH --mem-per-cpu={mem}MB
+#SBATCH --time={time}
 
-    module use {module_dir}
-    module load dspsr
-    module load psrchive
-    cd {workdir}
-    {script}
+module use {module_dir}
+module load dspsr
+module load psrchive
+cd {workdir}
+{script}
     """
-    shebag = '#!/bin/bash -l'
-    export = "NONE"
-    cluster = comp_config["cpuq_cluster"]
-    partition = comp_config["cpuq_partition"]
-    module_dir = comp_config["moduile_dir"]
-    mem = kwargs["memory"]
-    account=comp_config['group_account']["cpuq"]
-    outfile = kwargs["out_name"]
-    workdir = kwargs["workdir"]
-    script = kwargs["cmd"] + " " + kwargs["ops"]
-    tmpl = SLURM_TMPL.format(shebag=shebag, script=commands, outfile=outfile,
-                       cluster=cluster, partition=partition,
-                       export=export, account=account,
-                       module_dir=module_dir, mem=mem)
-    with open(kwags["batch_file"], "w") as f:
+    workdir = kwargs[]
+    template = {}
+    template["shebang"] = '#!/bin/bash -l'
+    template["export"] = "NONE"
+    template["cluster"] = comp_config["cpuq_cluster"]
+    template["partition"] = comp_config["cpuq_partition"]
+    template["module_dir"] = comp_config["module_dir"]
+    template["mem"] = kwargs["memory"]
+    template["account"] = comp_config['group_account']["cpuq"]
+    template["time"] = str(datetime.timedelta(hours=kwargs["time"]))
+    template["outfile"] = kwargs["out_name"]
+    template["workdir"] = kwargs["workdir"]
+    template["script"] = f"{kwargs['cmd']} {kwargs['ops']}"
+    tmpl = SLURM_TMPL.format(**template)
+    with open(kwargs["batch_name"], "w") as f:
         f.write(tmpl)
+    logger.info(f"Written task to file: {kwargs['batch_name']}")
 
 
 def submit_job(batch_file):
-    subprocess.run([f"sbatch {batch_file}"])
+    cmds = ["sbatch"]
+    cmds.append(batch_file)
+    subprocess.run(cmds)
 
 
 def jobsub_main(kwargs):
@@ -67,7 +72,7 @@ if __name__ ==  "__main__":
     cmdops.add_argument("--ops", type=str, required=True, help="Provide as a string in quotes any options you would like to use with the selected command. eg. '-D 50.0 -c 0.50625'")
 
     subops = parser.add_argument_group("Job Options")
-    subops.add_argument("--submit", type=bool, default=False, help="If True, will submit the job after creating the batch script")
+    subops.add_argument("--submit", action="store_true", help="If this tag is used, will submit the job after creating the batch script")
     subops.add_argument("--time", type=float, default=2.0, help="The time allocation for the job in hours")
     subops.add_argument("--job_name", type=str, default=None, help="The name of the job on the slurm queue")
     subops.add_argument("--memory", type=int, default=2048, help="The memory allocation for the job in MB.")
@@ -75,7 +80,7 @@ if __name__ ==  "__main__":
     writeops = parser.add_argument_group("Write Options")
     writeops.add_argument("--batch_name", type=str, default="batch_file.batch", help="The name of the resulting batch file")
     writeops.add_argument("--out_name", type=str, default=None, help="The name of the slurm output file")
-    writeops.add_argument("--workdir", type=str, default=".", help="The directory to run the job if necessary")
+    writeops.add_argument("--workdir", type=str, default="./", help="The directory to run the job if necessary")
 
     parser.add_argument("--loglvl", type=str, default="INFO", help="Logger verbosity level", choices=loglevels.keys())
 
@@ -86,5 +91,5 @@ if __name__ ==  "__main__":
     formatter = logging.Formatter(
         '%(asctime)s  %(filename)s  %(name)s  %(lineno)-4d  %(levelname)-9s :: %(message)s')
     ch.setFormatter(formatter)
-    kawrgs=vars(args)
+    kwargs=vars(args)
     jobsub_main(kwargs)
